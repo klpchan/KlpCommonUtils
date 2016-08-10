2c65247197c9119a3be7380c4c12200a1bb6d2b6
klpchan <fs.klpchan@gmail.com>
2016-08-10 21:27:09 +0800

    Delete InetworkResponse.java

--- src/com/klpchan/commonutils/network/InetworkResponse.java
+++
@@ -1,8 +0,0 @@
-package com.klpchan.commonutils.network;
-
-/**
- * Created by jinwook2.kim on 2015-12-15.
- */
-public interface InetworkResponse {
-    public void onPostNetwork(String response);
-}
